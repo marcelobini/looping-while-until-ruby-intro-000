83836c13d133ce674b1dfd042beee3058b6ba99a
Marcelo Bini <marcelobini@gmail.com>
2020-03-13 20:57:12 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
-def using_until
+def using_until 
   levitation_force = 6
   #your code here
-    
+  loop levitation_forcer do
+    puts "Wingardium Leviosa"
+    counter = counter+1
+  end  
 end
 
